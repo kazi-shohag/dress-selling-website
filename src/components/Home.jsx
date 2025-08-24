93c8e401880c94bf8e241b35801a24d0a96c120b
kazi-shohag <kazishohag1166@gmail.com>
2025-08-24 16:05:01 +0600

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        This is header section.
+      </header>
+      <main>
+        this is main section.
+      </main>
+      
+        <footer>
+          this is footer 
+        </footer>
+    </div>
   )
 }
 
